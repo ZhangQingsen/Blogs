7e0cce601e0b4a4fed9c880d5347a7a0391e399b
ZhangQingsen <1463769805zqs@gmail.com>
2023-10-18 00:42:17 -0400

    update intro 2 python

---
+++ posts/intro-to-python/.vdoc.py
@@ -0,0 +1,46 @@
+# type: ignore
+# flake8: noqa
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+print("Hello World!")
+#
+#
+#
+#
+#
+#
+#
+#
+
+
+
+
+# This is line #5   这是第5行
+#
+#
+#
+#
+#
+import numpy as np
+```
+#
+print(np.__version__)
+```
+#
+#
+#
+#
+#
